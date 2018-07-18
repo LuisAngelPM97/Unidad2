6317a801901ae9a1fed6fb3eda45623868a03107
15040111 <luisgunsanroses@hotmail.com>
2018-07-18 09:45:35 -0500

    Initial commit

---
+++ src/EditorDeTexto.java
@@ -0,0 +1,41 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+public class EditorDeTexto extends Application {
+
+    @FXML
+    private TextArea TextArea;
+
+    @FXML
+    void MenuAbiri(ActionEvent event) {
+
+    }
+
+    @FXML
+    void MenuCerrar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void MenuGuardar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void MenuGuardarComo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void MenuNuevo(ActionEvent event) {
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
